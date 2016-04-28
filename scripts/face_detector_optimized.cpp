bd9f23e19ff0e6c24b9552d4a988052824408b76
pgigioli <paul.m.gigioli@pwc.com>
2016-04-28 17:54:03 -0500

    modified:   launch/usb_cam.launch 	modified:   scripts/face_detector_optimized.cpp 	modified:   scripts/face_tracker.cpp

--- scripts/face_detector_optimized.cpp
+++ scripts/face_detector_optimized.cpp
@@ -11,12 +11,12 @@
 
 #include <iostream>
 #include <stdio.h>
-#include <typeinfo>
 
 using namespace std;
 using namespace cv;
 using namespace cv::gpu;
 
+// define global variables
 static const std::string OPENCV_WINDOW = "Face detection with GPU support";
 static const std::string CROPPED_WINDOW = "Face cropped";
 string face_cascade_name = "/home/ubuntu/haarcascades/GPU/haarcascade_frontalface_alt.xml";
@@ -27,12 +27,15 @@ Mat cv_ptr_copy;
 Point pt1;
 Point pt2;
 Size sz;
-int tryCascade = 1;
+int waitForCascade = 1;
+int tryCascade = 0;
 int tryTemplateMatching = 0;
-int steps = 0;
+int TMsteps = 0;
 Point searchROIpt1;
 Point searchROIpt2;
 Rect searchROI;
+int frame_width;
+int frame_height;
 
 class faceDetector
 {
@@ -62,6 +65,7 @@ public:
 	//cv::destroyWindow(CROPPED_WINDOW);
   }
 
+  //save a MAT image to the location /home/ubuntu/face_pictures
   int saveCroppedImg(Mat ROI_cropped, int ct, string title)
   {
 	stringstream ss;
@@ -80,6 +84,7 @@ public:
 	return ct;
   }
 
+  // publish the cropped ROI for a deep learning model
   void publishCroppedImg(Mat ROI_cropped)
   {
 	cv_bridge::CvImage img_bridge;
@@ -92,32 +97,25 @@ public:
         image_pub_.publish(img_msg);
   }
 
-  //cv_bridge::CvImagePtr drawCenterZone(cv_bridge::CvImagePtr cv_ptr)
+  // draw center zone for face tracker
   Mat drawCenterZone(Mat cv_ptr_copy)
   {
-	int image_width;
-        int image_height;
 	float scale = 0.40;
 
-        ros::param::get("/usb_cam/image_width", image_width);
-        ros::param::get("/usb_cam/image_height", image_height);
-
-        float zone_x_min = (image_width * 0.5) - (image_width * scale * 0.5);
-        float zone_x_max = (image_width * 0.5) + (image_width * scale * 0.5);
-        float zone_y_min = (image_height * 0.5) - (image_height * scale * 0.5);
-        float zone_y_max = (image_height * 0.5) + (image_height * scale * 0.5);
+        float zone_x_min = (frame_width * 0.5) - (frame_width * scale * 0.5);
+        float zone_x_max = (frame_width * 0.5) + (frame_width * scale * 0.5);
+        float zone_y_min = (frame_height * 0.5) - (frame_height * scale * 0.5);
+        float zone_y_max = (frame_height * 0.5) + (frame_height * scale * 0.5);
 
         rectangle(cv_ptr_copy, Point(zone_x_min, zone_y_min),
                    Point(zone_x_max, zone_y_max), Scalar(255,0,255));
 	return cv_ptr_copy;
   }
 
-  geometry_msgs::Point setCenterPoint(float ROI_center_x, float ROI_center_y,
-					Rect* cfaces, int detect_num)
+  // set the center point for n number of faces
+  geometry_msgs::Point setCenterPoint(Rect* cfaces, int detect_num)
   {
 	geometry_msgs::Point ROI_center;
-	ROI_center.x = ROI_center_x;
-        ROI_center.y = ROI_center_y;
 
         int x_total = 0;
         int y_total = 0;
@@ -137,16 +135,14 @@ public:
   	return ROI_center;
   }
 
-  void set_pt1(float x, float y)
+  // set top left and bottom right corners of new ROI to draw face box
+  void setROIcorners(float x1, float y1, float x2, float y2)
   {
-	pt1 = Point(x, y);
+	pt1 = Point(x1, y1);
+	pt2 = Point(x2, y2);
   }
 
-  void set_pt2(float x, float y)
-  {
-	pt2 = Point(x, y);
-  }
-
+  // get a small piece of the face frame to use in template matching
   Mat getFaceTemplate(Mat ROI_cropped)
   {
 	Size ROIsize = ROI_cropped.size();
@@ -163,90 +159,143 @@ public:
 	return faceTemplate;
   }
 
-  void templateMatching(Mat ROI_cropped, Mat cv_ptr_copy, Point pt1)
+  // check if template matching is failing or not
+  int checkThreshold(Mat matchingResult)
   {
-	Mat image = cv_ptr_copy;
-	float scale_ratio = 0.25;
+	float threshold = 0.3;
+	double minCheck, maxCheck;
+	Point minLocCheck, maxLocCheck;
+        minMaxLoc(matchingResult, &minCheck, &maxCheck, &minLocCheck, &maxLocCheck);
+	if ( minCheck > threshold ) return 1;
+	else return 0;
+  }
 
+  Rect setSearchROI(Mat ROI_cropped, Point pt1)
+  {
+	// define scale to increase ROI dimensions for the search ROI
+	float scale_ratio = 0.5;
 	int widthIncrease = scale_ratio*ROI_cropped.cols;
 	int heightIncrease = scale_ratio*ROI_cropped.rows;
 
-	searchROIpt1 = Point(pt1.x - widthIncrease*0.5, pt1.y - heightIncrease*0.5);
-	searchROIpt2 = Point(searchROIpt1.x + ROI_cropped.cols + widthIncrease,
-				searchROIpt1.y + ROI_cropped.rows + heightIncrease);
+        // define new search ROI box corners
+        searchROIpt1 = Point(pt1.x - widthIncrease*0.5, pt1.y - heightIncrease*0.5);
+        searchROIpt2 = Point(searchROIpt1.x + ROI_cropped.cols + widthIncrease,
+                                searchROIpt1.y + ROI_cropped.rows + heightIncrease);
 
-	int frame_width;
-	int frame_height;
+	// redefine searchROI coordinates if they are outside frame dimensions
+        if (searchROIpt1.x < 0) { searchROIpt1.x = 0; }
+        if (searchROIpt1.y < 0) { searchROIpt1.y = 0; }
+        if (searchROIpt1.x > frame_width) { searchROIpt1.x = frame_width; }
+        if (searchROIpt1.y > frame_height) { searchROIpt1.y = frame_height; }
 
-	ros::param::get("/usb_cam/image_width", frame_width);
-        ros::param::get("/usb_cam/image_height", frame_height);
-	if (steps < 10 && pt1.x > 0 && pt1.y > 0)
+        if (searchROIpt2.x < 0) { searchROIpt2.x = 0; }
+        if (searchROIpt2.y < 0) { searchROIpt2.y = 0; }
+        if (searchROIpt2.x > frame_width) { searchROIpt2.x = frame_width; }
+        if (searchROIpt2.y > frame_height) { searchROIpt2.y = frame_height; }
+
+	// define search ROI dimensions and draw rectangle on frame
+	searchROI = Rect(searchROIpt1.x, searchROIpt1.y,
+             searchROIpt2.x - searchROIpt1.x, searchROIpt2.y - searchROIpt1.y);
+	rectangle(cv_ptr_copy, searchROIpt1, searchROIpt2, Scalar(0,255,0), 1, 8, 0);
+	return searchROI;
+  }
+
+  // template matching method
+  void templateMatching(Mat ROI_cropped, Mat cv_ptr_copy, Point pt1)
+  {
+	// create copy of full frame
+	Mat image = cv_ptr_copy;
+
+	// run template matching TMsteps times and then flash cascade
+	if (TMsteps < 10 && pt1.x > 0 && pt1.y > 0)
         {
-	  if (searchROIpt1.x < 0) { searchROIpt1.x = 0; }
-	  if (searchROIpt1.y < 0) { searchROIpt1.y = 0; }
-	  if (searchROIpt1.x > frame_width) { searchROIpt1.x = frame_width; }
-	  if (searchROIpt1.y > frame_height) { searchROIpt1.y = frame_height; }
+	  // call function to set search ROI dimensions
+	  searchROI = setSearchROI(ROI_cropped, pt1);
 
-          if (searchROIpt2.x < 0) { searchROIpt2.x = 0; }
-          if (searchROIpt2.y < 0) { searchROIpt2.y = 0; }
-          if (searchROIpt2.x > frame_width) { searchROIpt2.x = frame_width; }
-          if (searchROIpt2.y > frame_height) { searchROIpt2.y = frame_height; }
-
-	  searchROI = Rect(searchROIpt1.x, searchROIpt1.y,
-	  		searchROIpt2.x - searchROIpt1.x, searchROIpt2.y - searchROIpt1.y);
-	  rectangle(cv_ptr_copy, searchROIpt1, searchROIpt2, Scalar(0,255,0), 1, 8, 0);
-
+	  // cut out search ROI image from full frame
 	  Mat searchImage = image(searchROI);
+
+	  // call function to define face template as a small piece of the original ROI
 	  Mat faceTemplate = getFaceTemplate(ROI_cropped);
 
+	  // create blank Mat array to place results
 	  Mat matchingResult;
-	  matchingResult.create( image.rows, image.cols, CV_8UC3 );
 
+	  // run template matching functions
 	  matchTemplate(searchImage, faceTemplate, matchingResult, CV_TM_SQDIFF_NORMED);
+
+	  // call function to determine any significant matches were found
+	  int TMfailure = checkThreshold(matchingResult);
+
+	  // if no good matches were found, turn off template matching and wait for
+	  // cascade classifier
+	  if (TMfailure == 1)
+	  {
+	    waitForCascade = 1;
+	    return;
+	  }
+
+	  // normalize results between 0 and 1
 	  normalize(matchingResult, matchingResult, 0, 1, NORM_MINMAX, -1, Mat());
+
+	  // determine min and max value and find the locations of each
 	  double min, max;
 	  Point minLoc, maxLoc;
 	  minMaxLoc(matchingResult, &min, &max, &minLoc, &maxLoc);
 
-Size temp_size = ROI_cropped.size();
+	  // adjust new ROI coordinates to full frame
+	  minLoc.x += searchROI.x - faceTemplate.cols*0.5;
+	  minLoc.y += searchROI.y - faceTemplate.rows*0.5;
 
-	  minLoc.x += pt1.x - widthIncrease*0.5 - temp_size.width*0.25;
-	  minLoc.y += pt1.y - heightIncrease*0.5 - temp_size.height*0.25;;
+	  // add smoother to ROI movement.  If the new ROI coordinates move too much
+	  // limit their new location to a small buffer
+	  if ((minLoc.x - pt1.x) > 20) {minLoc.x = pt1.x + 20;}
+	  if ((minLoc.y - pt1.y) > 20) {minLoc.y = pt1.y + 20;}
 
+	  // draw new boundary around estimate face position
 	  Point vertex1 = Point(minLoc.x, minLoc.y);
 	  Point vertex2 = Point(minLoc.x + ROI_cropped.cols, minLoc.y + ROI_cropped.rows);
 	  rectangle(cv_ptr_copy, vertex1, vertex2, Scalar(255));
 
+	  // draw center zone for face tracker
 	  drawCenterZone(cv_ptr_copy);
 
-	  Point templateMatchingCenter = Point(vertex1.x + ROI_cropped.cols*0.5, vertex1.y + ROI_cropped.rows*0.5);
+	  // define and draw center point of new ROI for face tracker
+	  Point templateMatchingCenter = Point(vertex1.x + ROI_cropped.cols*0.5,
+						vertex1.y + ROI_cropped.rows*0.5);
 	  geometry_msgs::Point faceTrackingPoint;
 	  faceTrackingPoint.x = templateMatchingCenter.x;
 	  faceTrackingPoint.y = templateMatchingCenter.y;
 
 	  circle(cv_ptr_copy, templateMatchingCenter, 2, Scalar(255,0,255), 2, 8, 0 );
 
+	  // publish center point for the face tracker
 	  ROI_coordinate_pub_.publish(faceTrackingPoint);
 
+	  // show new frame with annotations
 	  imshow( OPENCV_WINDOW, cv_ptr_copy );
           cv::waitKey(3);
 
-	  set_pt1(vertex1.x, vertex1.y);
-	  set_pt2(vertex2.x, vertex2.y);
+	  // set new ROI corners as global values
+	  setROIcorners(vertex1.x, vertex1.y, vertex2.x, vertex2.y);
 
-	  steps++;
+	  // increment template matching interval
+	  TMsteps++;
 	}
 	else
 	{
-	  steps = 0;
+	  // turn off template matching and try cascade classifier
+	  TMsteps = 0;
 	  tryTemplateMatching = 0;
 	  tryCascade = 1;
 	}
   }
 
+  // cascade classifier method
   void cascadeClassifier(Mat cv_ptr_copy)
   {
-          GpuMat faces;
+          // convert frame into grayscale and detect the number of faces identified
+	  GpuMat faces;
           Mat frame_gray;
 
           cvtColor( cv_ptr_copy, frame_gray, CV_BGR2GRAY );
@@ -256,24 +305,32 @@ Size temp_size = ROI_cropped.size();
 
           int detect_num = face_cascade.detectMultiScale(gray_gpu, faces, 1.1, 2, Size(30,30));
 
+	  // if no faces detected, either keep trying or switch to template matching
 	  if (detect_num == 0)
 	  {
-	    tryCascade = 0;
-	    tryTemplateMatching = 1;
+	    if (waitForCascade == 1)
+	    {
+		tryCascade = 1;
+		tryTemplateMatching = 0;
+	    }
+	    else
+	    {
+		tryCascade = 0;
+		tryTemplateMatching = 1;
+	    }
 	  }
+	  // if a face is detected, keep trying for the next frame
 	  else
 	  {
+	    waitForCascade = 0;
 	    tryCascade = 1;
 	    tryTemplateMatching = 0;
 	  }
 
+	  // retrieve detected faces and place dimensions in cfaces
           Mat obj_host;
           faces.colRange(0, detect_num).download(obj_host);
           Rect* cfaces = obj_host.ptr<Rect>();
-
-          float ROI_center_x;
-          float ROI_center_y;
-          int face_exists = 0;
 
           //loop to draw ROI boxes
           for( size_t i = 0; i < detect_num; i++ )
@@ -282,41 +339,37 @@ Size temp_size = ROI_cropped.size();
             sz = cfaces[i].size();
             pt2 = Point(pt1.x+sz.width, pt1.y+sz.height);
 
+	    // define ROI box and draw on frame
             ROI_cropped = cv_ptr_copy(Rect(pt1.x, pt1.y, sz.width, sz.height));
 
             rectangle(cv_ptr_copy, pt1, pt2, Scalar(255));
 
+	    // save cropped image, publish ROI frame for deep learning node, and publish
+	    // center for face tracker
             if (ROI_cropped.empty() == false)
             {
-		ct = saveCroppedImg(ROI_cropped, ct, "cropped_");
-                //imshow(CROPPED_WINDOW, ROI_cropped);
+		// call function to save cropped image
+		//ct = saveCroppedImg(ROI_cropped, ct, "cropped_");
+
+		//imshow(CROPPED_WINDOW, ROI_cropped);
                 //cv::waitKey(3);
 
+		// call function to publish the cropped ROI for deep learning
                 publishCroppedImg(ROI_cropped);
-            }
 
-            float ROI_center_x = cfaces[i].x + sz.width*0.5;
-            float ROI_center_y = cfaces[i].y + sz.height*0.5;
+		// handle multiple faces by placing center point at mean of all ROIs
+		geometry_msgs::Point ROI_center = setCenterPoint(cfaces, detect_num);
 
-            face_exists = 1;
+                Point face_center(ROI_center.x, ROI_center.y);
+                circle(cv_ptr_copy, face_center, 2, Scalar(255,0,255), 2, 8, 0 );
+            	ROI_coordinate_pub_.publish(ROI_center);
+	    }
 
             // define center zone for face tracking
             drawCenterZone(cv_ptr_copy);
 	  }
 
-          // Handle multiple faces by placing the center point at the mean of all ROIs
-          geometry_msgs::Point ROI_center = setCenterPoint(ROI_center_x, ROI_center_y,
-                                                                cfaces, detect_num);
-
-          Point face_center(ROI_center.x, ROI_center.y);
-          circle(cv_ptr_copy, face_center, 2, Scalar(255,0,255), 2, 8, 0 );
-
-          if (face_exists == 1)
-          {
-            face_exists = 0;
-            ROI_coordinate_pub_.publish(ROI_center);
-          }
-
+	  // display all annotations
           imshow( OPENCV_WINDOW, cv_ptr_copy );
           cv::waitKey(3);
   }
@@ -352,14 +405,23 @@ Size temp_size = ROI_cropped.size();
 
 	  //int a = saveCroppedImg(cv_ptr_copy, ct, "full_image");
 
-	  //call cascade face detector
+	  // if waiting for cascade to detect face, turn off template matching and keep trying
+	  // cascade
+	  if (waitForCascade == 1)
+	  {
+	    tryCascade = 1;
+	    tryTemplateMatching = 0;
+	  }
+
+	  // call cascade face classifier
 	  if (tryCascade == 1) { cascadeClassifier(cv_ptr_copy); }
 
-	  //check how long it took
+	  //check how long it took for cascade classifier
 	  t=((double)getTickCount()-t)/getTickFrequency();
           totalT += t;
           frmCnt++;
 
+	  // call template matching
 	  if (tryTemplateMatching == 1) { templateMatching(ROI_cropped, cv_ptr_copy, pt1); }
 
 	  //image_pub_.publish(cv_ptr->toImageMsg());
@@ -383,6 +445,11 @@ int main(int argc, char** argv)
   // Load cascade
   if( !face_cascade.load( face_cascade_name ) ){ std::cout << "Error loading face cascade" << endl; };
 
+  // get frame dimensions from ROS
+  ros::param::get("/usb_cam/image_width", frame_width);
+  ros::param::get("/usb_cam/image_height", frame_height);
+
+  // call face detector class
   faceDetector fd;
   ros::spin();
   return 0;
--- scripts/face_tracker.cpp
+++ scripts/face_tracker.cpp
@@ -6,6 +6,9 @@
 
 using namespace std;
 
+float image_width;
+float image_height;
+
 void callback(const geometry_msgs::Point::ConstPtr& msg)
 {
 
@@ -16,51 +19,49 @@ void callback(const geometry_msgs::Point::ConstPtr& msg)
   cout << "y: " << center_y << endl;
   cout << " " << endl;
 
-  float image_width;
-  float image_height;
-  ros::param::get("/usb_cam/image_width", image_width);
-  ros::param::get("/usb_cam/image_height", image_height);
-
   float zone_x_min = (image_width * 0.5) - (image_width * 0.4 * 0.5);
   float zone_x_max = (image_width * 0.5) + (image_width * 0.4 * 0.5);
   float zone_y_min = (image_height * 0.5) - (image_height * 0.4 * 0.5);
   float zone_y_max = (image_height * 0.5) + (image_height * 0.4 * 0.5);
 
   float delay = 0.1;
+  string moveIncr = "320";
+
+  stringstream ss;
 
   // Zone 1
   if (center_y < zone_y_min && center_x > zone_x_min && center_x < zone_x_max)
   {
-	string command = "uvcdynctrl -s \"Tilt (relative)\" -- -320";
+	string command = "uvcdynctrl -s \"Tilt (relative)\" -- -" + moveIncr;
         system(command.c_str());
   }
 
   // Zone 2
   if (center_y > zone_y_max && center_x > zone_x_min && center_x < zone_x_max)
   {
-	string command = "uvcdynctrl -s \"Tilt (relative)\" -- 320";
-        system(command.c_str());
+	string command = "uvcdynctrl -s \"Tilt (relative)\" -- " + moveIncr;
+	system(command.c_str());
   }
 
   // Zone 3
   if (center_x < zone_x_min && center_y > zone_y_min && center_y < zone_y_max)
   {
-	string command = "uvcdynctrl -s \"Pan (relative)\" -- 320";
+	string command = "uvcdynctrl -s \"Pan (relative)\" -- " + moveIncr;
         system(command.c_str());
   }
 
   // Zone 4
   if (center_x > zone_x_max && center_y > zone_y_min && center_y < zone_y_max)
   {
-	string command = "uvcdynctrl -s \"Pan (relative)\" -- -320";
+	string command = "uvcdynctrl -s \"Pan (relative)\" -- -" + moveIncr;
         system(command.c_str());
   }
 
   // Zone 5
   if (center_y < zone_y_min && center_x < zone_x_min)
   {
-	string command1 = "uvcdynctrl -s \"Tilt (relative)\" -- -320";
-	string command2 = "uvcdynctrl -s \"Pan (relative)\" -- 320";
+	string command1 = "uvcdynctrl -s \"Tilt (relative)\" -- -" + moveIncr;
+	string command2 = "uvcdynctrl -s \"Pan (relative)\" -- " + moveIncr;
 	system(command1.c_str());
 	ros::Duration(delay).sleep();
 	system(command2.c_str());
@@ -70,8 +71,8 @@ void callback(const geometry_msgs::Point::ConstPtr& msg)
   // Zone 6
   if (center_y < zone_y_min && center_x > zone_x_max)
   {
-	string command1 = "uvcdynctrl -s \"Tilt (relative)\" -- -320";
-	string command2 = "uvcdynctrl -s \"Pan (relative)\" -- -320";
+	string command1 = "uvcdynctrl -s \"Tilt (relative)\" -- -" + moveIncr;
+	string command2 = "uvcdynctrl -s \"Pan (relative)\" -- -" + moveIncr;
 	system(command1.c_str());
         ros::Duration(delay).sleep();
         system(command2.c_str());
@@ -81,8 +82,8 @@ void callback(const geometry_msgs::Point::ConstPtr& msg)
   // Zone 7
   if (center_y > zone_y_max && center_x < zone_x_min)
   {
-	string command1 = "uvcdynctrl -s \"Tilt (relative)\" -- 320";
-	string command2 = "uvcdynctrl -s \"Pan (relative)\" -- 320";
+	string command1 = "uvcdynctrl -s \"Tilt (relative)\" -- " + moveIncr;
+	string command2 = "uvcdynctrl -s \"Pan (relative)\" -- " + moveIncr;
 	system(command1.c_str());
         ros::Duration(delay).sleep();
         system(command2.c_str());
@@ -92,8 +93,8 @@ void callback(const geometry_msgs::Point::ConstPtr& msg)
   //Zone 8
   if (center_y > zone_y_max && center_x > zone_x_max)
   {
-	string command1 = "uvcdynctrl -s \"Tilt (relative)\" -- 320";
-	string command2 = "uvcdynctrl -s \"Pan (relative)\" -- -320";
+	string command1 = "uvcdynctrl -s \"Tilt (relative)\" -- " + moveIncr;
+	string command2 = "uvcdynctrl -s \"Pan (relative)\" -- -" + moveIncr;
 	system(command1.c_str());
         ros::Duration(delay).sleep();
         system(command2.c_str());
@@ -106,6 +107,10 @@ int main (int argc, char **argv)
 {
   ros::init(argc, argv, "face_tracker");
   ros::NodeHandle n;
+
+  ros::param::get("/usb_cam/image_width", image_width);
+  ros::param::get("/usb_cam/image_height", image_height);
+
   ros::Subscriber sub = n.subscribe("ROI_coordinate", 1, callback);
   ros::spin();
   return 0;
